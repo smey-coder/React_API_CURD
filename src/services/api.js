e41d9dcd8dd595c45d20d35b18c179f2212cf5d3
Your Name <sanreaksmey01@gmail.com>
2026-07-03 14:26:27 +0700

    sd

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://my-api-connect-with-react-3.onrender.com",
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
 export default API;
